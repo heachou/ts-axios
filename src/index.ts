2c690c91c51b7ea61c385856014c8946dee8b831
heachou <1244239990@qq.com>
2020-06-15 17:28:18 +0800

    feat(all the file): 基础功能实现
    处理请求url,处理body,处理header，处理响应的data

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,39 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helps/url'
+import { transformRequest, transformResponse } from './helps/data'
+import { processHeaders } from './helps/headers'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeader(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): string {
+  return transformRequest(config.data)
+}
+
+function transformHeader(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
