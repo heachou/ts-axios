70de9bb925a9c04811963ac0c20f27ab49898a74
heachou <1244239990@qq.com>
2020-06-18 17:55:18 +0800

    feat(all the file): 函数重载
    函数重载 同时支持 axios(url,config) 和 axios(config)

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -2,7 +2,16 @@ import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    // 默认可以传入一个或者两个参数
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
